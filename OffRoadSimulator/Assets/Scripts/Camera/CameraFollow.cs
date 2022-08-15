6822596e619e7074bb93c164877f056a2ddaca5c
Sethunath <sethunath3@gmail.com>
2022-08-15 22:52:24 +0530

    Camera follow system

--- OffRoadSimulator/Assets/Scripts/Camera/CameraFollow.cs
+++ OffRoadSimulator/Assets/Scripts/Camera/CameraFollow.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] Vector3 offset;
+    [SerializeField] Transform target;
+    [SerializeField] float translationSpeed;
+    [SerializeField] float rotationSpeed;
     private void FixedUpdate()
     {
         HandleTranslation();
@@ -12,11 +16,14 @@ public class CameraFollow : MonoBehaviour
 
     void HandleTranslation()
     {
-
+        var targetPosition = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translationSpeed * Time.deltaTime);
     }
 
     void HandleRotation()
     {
-
+        var direction = target.position - transform.position;
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
